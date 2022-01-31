c8949514e33a67aace0fa26022ef253049f95672
ChirpyMisha <chirpymishabs@gmail.com>
2022-01-31 12:40:17 +0100

    Forgot to commit this file

--- CustomParticles/Utils/ParticlesUtils.cs
+++ CustomParticles/Utils/ParticlesUtils.cs
@@ -17,26 +17,24 @@ namespace CustomParticles.Utils
 
 		public static void SetCustomParticles(ParticleSystem ps, ParticleSettings settings)
 		{
-			// Load and set texture
-			LoadAndSetParticleImage(ps, settings.fileName);
-
-			// Enable random textures
-			if (settings.frameCount > 1)
-				EnableAndSetTextureSheetAnimation(ps, settings);
-		}
-
-		public static void LoadAndSetParticleImage(ParticleSystem ps, string imgPath)
-		{
-			string fullPath = ImgUtils.DefaultPath + imgPath;
-
 			// If the texture exists, change the main texture of the particle system.
-			if (File.Exists(fullPath))
+			if (ImgUtils.ImgExists(settings.fileName))
 			{
-				Material material = GetMaterial(ps);
-				material.mainTexture = ImgUtils.LoadTexture(fullPath);
+				// Load and set texture
+				LoadAndSetParticleImage(ps, settings.fileName);
+
+				// Enable random textures
+				if (settings.imgCountX > 1 || settings.imgCountY > 1)
+					EnableAndSetTextureSheetAnimation(ps, settings);
 			}
 			else
-				Plugin.Log.Warn($"Failed to load image from: {fullPath}");
+				Plugin.Log.Warn($"Failed to load image from: {ImgUtils.FullPath(settings.fileName)}");
+		}
+
+		public static void LoadAndSetParticleImage(ParticleSystem ps, string fileName)
+		{
+			Material material = GetMaterial(ps);
+			material.mainTexture = ImgUtils.LoadTexture(fileName);
 		}
 
 		public static void EnableAndSetTextureSheetAnimation(ParticleSystem partSys, ParticleSettings settings)
@@ -49,11 +47,58 @@ namespace CustomParticles.Utils
 			texSheetAnimation.numTilesX = settings.imgCountX;
 			texSheetAnimation.numTilesY = settings.imgCountY;
 			texSheetAnimation.timeMode = settings.mode;
-			int frameCount = settings.frameCount > 0 ? settings.frameCount : settings.imgCountX * settings.imgCountY;
-			texSheetAnimation.startFrame = new MinMaxCurve(0, frameCount);
-			texSheetAnimation.fps = settings.fps;
-			texSheetAnimation.cycleCount = settings.cycleCount;
+			if (settings.mode == ParticleSystemAnimationTimeMode.Lifetime)
+			{
+				texSheetAnimation.startFrame = new MinMaxCurve(0, settings.imgCountX*settings.imgCountY);
+				texSheetAnimation.fps = 0;
+				texSheetAnimation.cycleCount = 0;
+			}
+			else if (settings.mode == ParticleSystemAnimationTimeMode.FPS)
+			{
+				
+				if (settings.fps == 0)
+				{
+					texSheetAnimation.timeMode = ParticleSystemAnimationTimeMode.Lifetime;
+					texSheetAnimation.startFrame = new MinMaxCurve(0, settings.imgCountX * settings.imgCountY);
+					texSheetAnimation.cycleCount = 1;
+				}
+				else
+				{
+					texSheetAnimation.startFrame = new MinMaxCurve(0, 0);
+					texSheetAnimation.fps = settings.fps;
+				}
+			}
 			// Show your accuracy as a percentage without drops from misses or a lowered combo multiplier. (To use the counter in-game requires Counters+ to be installed).
 		}
+
+		//public static void SaveTexture(ParticleSystem partSys, string fileName)
+		//{
+		//	Material material = GetMaterial(partSys);
+		//	SaveTextureAsPNG((Texture2D)material.mainTexture, ImgUtils.DefaultPath + fileName);
+		//}
+		//public static void SaveTextureAsPNG(Texture2D _texture, string _fullPath)
+		//{
+		//	byte[] _bytes = createReadabeTexture2D(_texture).EncodeToPNG();
+		//	System.IO.File.WriteAllBytes(_fullPath, _bytes);
+		//	Debug.Log(_bytes.Length / 1024 + "Kb was saved as: " + _fullPath);
+		//}
+		//public static Texture2D createReadabeTexture2D(Texture2D texture2d)
+		//{
+		//	RenderTexture renderTexture = RenderTexture.GetTemporary(
+		//				texture2d.width,
+		//				texture2d.height,
+		//				0,
+		//				RenderTextureFormat.Default,
+		//				RenderTextureReadWrite.Linear);
+		//	Graphics.Blit(texture2d, renderTexture);
+		//	RenderTexture previous = RenderTexture.active;
+		//	RenderTexture.active = renderTexture;
+		//	Texture2D readableTextur2D = new Texture2D(texture2d.width, texture2d.height);
+		//	readableTextur2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+		//	readableTextur2D.Apply();
+		//	RenderTexture.active = previous;
+		//	RenderTexture.ReleaseTemporary(renderTexture);
+		//	return readableTextur2D;
+		//}
 	}
 }
